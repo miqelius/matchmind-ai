f1abd979e53a6f5c2377c028da935673acab05d4
Giorgi Mikeladze <_@gmail.com>
2026-09-19 23:24:45 +0200

    chore: remove root clutter and legacy files

--- test_redis.py
+++
@@ -1,7 +0,0 @@
-from upstash_redis import Redis
-
-redis = Redis(url="https://optimal-civet-69003.upstash.io", token="gQAAAAAAAQ2LAAIgcDI4MTIwM2M4MGMwYmI0yczUzYjBmYjQ0MDg3YTY0ZTYyYg")
-
-redis.set("foo", "bar")
-value = redis.get("foo")
-print("Retrieved from Upstash:", value)
